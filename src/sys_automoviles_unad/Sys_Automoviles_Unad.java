c65fe6225198bfb451d688b29832cc51e44015ed
Deyson19 <deiandresvent@hotmail.com>
2023-02-24 17:17:00 -0500

    Diseño listo...

---
+++ src/sys_automoviles_unad/Sys_Automoviles_Unad.java
@@ -0,0 +1,22 @@
+
+package sys_automoviles_unad;
+
+import Forms.frmLogin;
+
+/**
+ *
+ * @author Deyson Vente
+ */
+public class Sys_Automoviles_Unad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmLogin login = new frmLogin();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+    
+}
