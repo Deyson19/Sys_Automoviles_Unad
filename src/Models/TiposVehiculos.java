92f1ac645bb96768296116211d9a4d505d0bf693
Deyson19 <deiandresvent@hotmail.com>
2023-03-31 01:24:54 -0500

    Clase de 'TipoVehiculos' creada

---
+++ src/Models/TiposVehiculos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Deyson Vente
+ */
+public class TiposVehiculos {
+    private int idTipoVehiculo;
+    private String Nombre;
+
+    public TiposVehiculos() {
+    }
+
+    public int getIdTipoVehiculo() {
+        return idTipoVehiculo;
+    }
+
+    public void setIdTipoVehiculo(int idTipoVehiculo) {
+        this.idTipoVehiculo = idTipoVehiculo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+    
+}
